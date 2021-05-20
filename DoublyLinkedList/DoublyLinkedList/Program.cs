5692e8dae36fb817bdbab97110ca7de5d2d73e5e
lukejelse04 <52086083+lukejelse04@users.noreply.github.com>
2021-05-20 10:18:06 +0100

    Change of Program to To documents tests

--- DoublyLinkedList/DoublyLinkedList/Program.cs
+++ DoublyLinkedList/DoublyLinkedList/Program.cs
@@ -10,29 +10,13 @@ namespace DoublyLinkedList
     {
         static void Main(string[] args)
         {
-            LinkedList list = new LinkedList(50);
+            LinkedList list = new LinkedList();
 
             list.AddItemInOrder(2);
-            list.AddItemInOrder(3);
-            list.AddItemInOrder(0);
-            list.AddItemInOrder(20);
-            list.AddItemInOrder(2.56);
-            list.AddItemInOrder(-5);
+            list.AddItemInOrder(5);
+            list.AddItemInOrder("Test");
+            list.AddItemInOrder("5");
 
-            list.printList();
-            list.printListReverse();
-            Console.WriteLine(list.getItemData(0));
-            Console.WriteLine(list.getItemData(1));
-            Console.WriteLine(list.getItemData(3));
-            Console.WriteLine(list.getItemData(7));
-
-            list.removeItem(2);
-            list.printList();
-
-            list.removeItem(0);
-            list.printList();
-
-            list.removeItem(4);
             list.printList();
 
             Console.ReadLine();
