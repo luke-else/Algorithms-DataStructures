c942ce57ce486d1490422b856d506cf579c94310
luke-else <52086083+luke-else@users.noreply.github.com>
2021-11-18 17:40:29 +0000

    Reorganised Repo

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tree tree = new tree();
+
+            Random random = new Random();
+
+            for (var i = 0; i < 35; i++)
+            {
+                tree.Add(random.Next(0, 100));
+            }
+
+            while (true)
+            {
+                Console.Clear();
+                tree.Print(tree.Root);
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Delete node: ");
+
+                tree.Delete(Convert.ToInt32(Console.ReadLine()));
+                
+            }
+
+        }
+    }
+}
---
+++ BinaryTree/node.cs
@@ -0,0 +1,52 @@
+namespace BinaryTree
+{
+    public class node
+    {
+        public dynamic Data { get; set; }
+        private node Parent { get; set; }
+        private node Left { get; set; }
+        private node Right { get; set; }
+
+
+        //Print Method Data
+        public int StartPos;
+        public int Size { get { return Data.Length; } }
+        public int EndPos { get { return StartPos + Size; } set { StartPos = value - Size; } }
+
+
+        public node(dynamic _Data = null,
+                    node _Parent = null, 
+                    node _Left = null, 
+                    node _Right = null){
+
+            //New node for a Binary Tree. Values can be set to null if they are not present
+            Data = _Data;
+            Parent = _Parent;
+            Left = _Left;
+            Right = _Right;
+
+        }
+
+        public node GetParent(){
+            return Parent;
+        }
+        public void SetParent(node _Parent){
+            Parent = _Parent;
+        }
+
+        public node GetLeft(){
+            return Left;
+        }
+        public void SetLeft(node _Left){
+            Left = _Left;
+        }
+
+        public node GetRight(){
+            return Right;
+        }        
+        public void SetRight(node _Right){
+            Right = _Right;
+        }
+
+    }
+}
